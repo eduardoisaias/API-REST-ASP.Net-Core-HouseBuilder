5487e4f6b725d488d0416b6d92dcd70fd2efa74a
Eduardo <ing.eduardo.isaias@outlook.com>
2018-11-14 22:32:24 -0600

    Web Api v1.0.0

---
+++ API_REST_Adventureworks/Controllers/EmployeeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API_REST_Adventureworks.Models.HumanResources;
+using API_REST_Adventureworks.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+
+namespace API_REST_Adventureworks.Controllers
+{
+    [Route("api/employees")]
+    public class EmployeeController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+
+        [Produces("application/json")]
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var employees = db.Employees.ToList();
+                return Ok(employees);
+            } catch (Exception exe)
+            {
+                return BadRequest(exe);
+            }
+        }
+
+        [Produces("application/json")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var employees = db.Employees.Find(id);
+                return Ok(employees);
+            }
+            catch (Exception exe)
+            {
+                return BadRequest(exe);
+            }
+        }
+
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [HttpPost("")]
+        public async Task<IActionResult> Add([FromBody] Employee employee)
+        {
+            try
+            {
+                var employees = db.Employees.Add(employee);
+                db.SaveChanges();
+                return Ok(employee);
+            }
+            catch (Exception exe)
+            {
+                return BadRequest(exe);
+            }
+        }
+
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [HttpPut("")]
+        public async Task<IActionResult> update([FromBody] Employee employee)
+        {
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return Ok(employee);
+            }
+            catch (Exception exe)
+            {
+                return BadRequest(exe);
+            }
+        }
+
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            try
+            {
+                db.Employees.Remove(db.Employees.Find(id));
+                db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception exe)
+            {
+                return BadRequest(exe);
+            }
+        }
+    }
+}
---
+++ API_REST_Adventureworks/Models/HumanResources/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_REST_Adventureworks.Models.HumanResources
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
